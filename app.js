a9551f47ba46fc66d03272d3a6d020dfd7ea7137
Jan Kudrna <jan.kudrna.tt@gmail.com>
2024-09-26 20:15:25 +0200

    vytvoreni otazky napocet clenu a jeji validace

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-// Funkce pro dotazování uživatele
+// Funkce pro dotazování uživatele na pocet prikladu
 function zeptatSePP() {
   rl.question('Napis kolik mam vytvorit prikladu (napis cislo od 1 do 9)', (odpovedPP) => {
     // Zkontroluj, zda je odpověď platná
     if (odpovedPP >= 1 && odpovedPP <= 9) {
       console.log(`Děkuji, pocet prikladu bude: ${odpovedPP}`);
-      rl.close(); // Zavřít rozhraní, pokud je vstup platný
+      zeptatSePPro(); //rl.close(); // Zavřít rozhraní, pokud je vstup platný
     } else {
       console.log('Neplatný vstup. Zkus to znovu.'); // Zpráva o neplatném vstupu
       zeptatSePP(); // Znovu se ptát uživatele
@@ -20,5 +20,20 @@ function zeptatSePP() {
   });
 }
 
+// Funkce pro dotazování uživatele na pocet clenu v prikladech
+function zeptatSePPro() {
+    rl.question('Napis kolik ma mit kazdy priklad clenu (napis cislo od 1 do 9)', (odpovedPPro) => {
+      // Zkontroluj, zda je odpověď platná
+      if (odpovedPPro >= 1 && odpovedPPro <= 9) {
+        console.log(`Děkuji, pocet clenu bude: ${odpovedPPro}`);
+        rl.close(); // Zavřít rozhraní, pokud je vstup platný
+      } else {
+        console.log('Neplatný vstup. Zkus to znovu.'); // Zpráva o neplatném vstupu
+        zeptatSePPro(); // Znovu se ptát uživatele
+      }
+    });
+  }
+
 // Spusť funkci
 zeptatSePP();
+
