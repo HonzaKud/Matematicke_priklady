2c6ac3980c2a4c7ccf1d858d113c54c5fb41f8ed
Jan Kudrna <jan.kudrna.tt@gmail.com>
2024-10-02 11:33:40 +0200

    vypsani poctu prikladu podle inputu a vytvoreni globalnich promennych

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+const nahodne = Math.floor(Math.random()*9)+1; //generator nahodnych cisel mezi 1 az 9
+//console.log(nahodne); generator nahodnych cisel mezi 1 az 9
+
+let pocetprikladu = null;
+let pocetclenu = null;
+
 // Funkce pro dotazování uživatele na pocet prikladu
 function zeptatSePP() {
   rl.question('Napis kolik mam vytvorit prikladu (napis cislo od 1 do 9)', (odpovedPP) => {
     // Zkontroluj, zda je odpověď platná
     if (odpovedPP >= 1 && odpovedPP <= 9) {
       console.log(`Děkuji, pocet prikladu bude: ${odpovedPP}`);
+      pocetprikladu = odpovedPP;
       zeptatSePPro(); //rl.close(); // Zavřít rozhraní, pokud je vstup platný
     } else {
       console.log('Neplatný vstup. Zkus to znovu.'); // Zpráva o neplatném vstupu
@@ -26,6 +33,15 @@ function zeptatSePPro() {
       // Zkontroluj, zda je odpověď platná
       if (odpovedPPro >= 1 && odpovedPPro <= 9) {
         console.log(`Děkuji, pocet clenu bude: ${odpovedPPro}`);
+        pocetclenu = odpovedPPro;
+         // Generace poctu prikladu
+            for (let i = 0; i < pocetprikladu; i++) {
+                //const nahodne = Math.floor(Math.random()*9)+1; //generator nahodnych cisel mezi 1 az 9
+                console.log(`priklad ${i + 1}: ${nahodne} ${nahodne}`); //generator nahodnych cisel mezi 1 az 9
+                console.log(pocetclenu);
+                console.log(pocetprikladu);
+            }
+        
         rl.close(); // Zavřít rozhraní, pokud je vstup platný
       } else {
         console.log('Neplatný vstup. Zkus to znovu.'); // Zpráva o neplatném vstupu
@@ -36,4 +52,3 @@ function zeptatSePPro() {
 
 // Spusť funkci
 zeptatSePP();
-
