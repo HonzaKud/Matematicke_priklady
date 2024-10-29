481ba587e38dcfb580d487db48a060284a385bbc
Jan Kudrna <jan.kudrna.tt@gmail.com>
2024-10-29 16:57:08 +0100

    ubrani funkci ze souboru app.js, ktere nebylo potreba volat z hlavniho souboru

--- app.js
+++ app.js
@@ -8,11 +8,8 @@ const rl = readline.createInterface({
 
 let numberOfExamples = null; //Promenna na pocet prikladu, ktere vyplni uzivatel, tahle promenna se pouziva u hodne funkci
 let numberOfTerms = null; // Promenna na pocet clenu, tohle take vyplni uzivatel
-let resultsArray = []; // Pole do ktereho se ukladaji spravne vysledky prikladu, ktere spocita pocitac
-let userAnswers = []; // Pole do ktereho se uklada odpovedi, ktere vyplni uzivatel
 
-const { generateRandomNumber, generateRandomOperator } = require('./randomGenerators'); // zavola funkce ze souboru randomGenerate
-const { askForNumberOfExamples, askForNumberOfTerms, askForResult } = require('./userInput'); // zavola funkce ze souboru userInput
+const { askForNumberOfExamples, askForNumberOfTerms } = require('./userInput'); // zavola funkce ze souboru userInput
 const { compareArrays } = require('./comparison'); //
 const { createExamples } = require('./exampleGenerator'); //
 
